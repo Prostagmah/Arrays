9761ce23ba475a62f7145026d43eacb6ee15738b
Артур <Suppermen1012@gmail.com>
2021-11-29 16:37:38 +0300

    Output of even numbers

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace hz_lol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] a = new int[500];
+            for (int i = 0; i < 500; i++)
+            {
+                a[i] = rnd.Next();
+            }
+            for (int i = 0; i < 500; i++)
+            {
+                if (a[i]%2 == 0)
+                {
+                    Console.WriteLine(a[i] + "");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
